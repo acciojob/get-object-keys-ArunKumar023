e21fd61beb6a5f562ee6d743241da50af63c4c11
Arun kumar <106857719+ArunKumar023@users.noreply.github.com>
2023-04-25 06:45:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+// Define the student object
+const student = {
+  name: 'John',
+  age: 20,
+  major: 'Computer Science'
+};
+
+// Add the getKeys method to the Object prototype
+Object.prototype.getKeys = function() {
+  const keys = [];
+  for (let key in this) {
+    if (this.hasOwnProperty(key)) {
+      keys.push(key);
+    }
+  }
+  return keys;
+};
+
+// Call the getKeys method on the student object
+const keys = student.getKeys();
+console.log(keys); 
